7a34adbb034d71acceccc747f6404108124beafa
Valentin Brosseau <c4software@gmail.com>
2018-10-28 22:36:05 +0100

    Add view for scan nfc tag

--- src/views/Nfc.vue
+++ src/views/Nfc.vue
@@ -1,11 +1,39 @@
 <template>
-    <v-container align-center justify-center grid-list-md fill-height>
-        <div>Todo NFC</div>
+    <v-container align-center justify-center grid-list-md fill-height text-xs-center>
+        <div>
+            <v-fade-transition mode="out-in">
+                <v-icon color="success" size="50" v-if="scanSuccess">check_circle</v-icon>
+                <v-progress-circular indeterminate size="50" v-else color="primary" />
+            </v-fade-transition>
+
+            <p class="pa-4">{{$t('nfc.waiting')}}</p>
+        </div>
+
+            <v-alert class="bottomToast" transition="fade-transition" :value="scanSuccess" type="success">
+                {{$t('nfc.success')}}
+            </v-alert>
+
     </v-container>
 </template>
 
 <script>
 export default {
-  name: "nfc"
+  name: "nfc",
+  data() {
+    return {
+      scanSuccess: false
+    };
+  }
 };
 </script>
+
+<style scoped>
+.bottomToast {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  margin: -1px;
+}
+</style>
+
