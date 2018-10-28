022ae35ce2a54684159a124444b72c09f17845a4
Valentin Brosseau <c4software@gmail.com>
2018-10-28 22:39:50 +0100

    Handle state of scan

--- src/views/Nfc.vue
+++ src/views/Nfc.vue
@@ -23,6 +23,17 @@ export default {
     return {
       scanSuccess: false
     };
+  },
+  watch: {
+    scanSuccess(state) {
+      if (state) {
+        // If state success
+        setTimeout(() => {
+          // After delay reset the state to waiting new scan
+          this.scanSuccess = false;
+        }, 3000);
+      }
+    }
   }
 };
 </script>
