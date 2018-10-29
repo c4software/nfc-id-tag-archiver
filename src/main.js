7f98551d1cae935e3928559718ec733a26b7ac31
Valentin Brosseau <c4software@gmail.com>
2018-10-29 08:30:05 +0100

    Add cordova deviceready propagation

--- src/main.js
+++ src/main.js
@@ -2,12 +2,26 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./plugins/vuetify";
 import router from "./router/";
-import i18n from './i18n'
+import i18n from "./i18n";
 
-Vue.config.productionTip = true;
+function init() {
+  Vue.config.productionTip = true;
+  new Vue({
+    router,
+    i18n,
+    render: h => h(App)
+  }).$mount("#app");
+}
 
-new Vue({
-  router,
-  i18n,
-  render: h => h(App)
-}).$mount("#app");
+// Wait for the deviceready event to start the render
+document.addEventListener("deviceready", () => {
+  // eslint-disable-next-line
+  console.log("Ready, Render the App");
+  init();
+});
+
+// If we are not in Cordova, manually trigger the deviceready event
+const isCordovaApp = typeof window.cordova !== "undefined";
+if (!isCordovaApp) {
+  document.dispatchEvent(new CustomEvent("deviceready", {}));
+}
