bdf4fa9bf037e33804f1d6cb60246e8a93380f72
Valentin Brosseau <c4software@gmail.com>
2018-10-30 13:48:43 +0100

    Reindent and add the v-alert on QRCode view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,15 +5,19 @@
         <v-flex xs12 text-xs-center>
           <v-icon size="120">nfc</v-icon>
         </v-flex>
+
         <v-flex xs6>
           <v-btn block large flat to="nfc">{{$t('nfcTitle')}}</v-btn>
         </v-flex>
+
         <v-flex xs6>
           <v-btn block large flat to="qrcode">{{$t('qrcodeTitle')}}</v-btn>
         </v-flex>
+
         <v-flex xs12>
           <v-btn flat block to="history" color="primary">{{$t('historyTitle')}}</v-btn>
         </v-flex>
+
         <v-btn fab flat class="settingsAction">
           <v-icon>settings</v-icon>
         </v-btn>
--- src/views/Qrcode.vue
+++ src/views/Qrcode.vue
@@ -1,14 +1,35 @@
 <template>
   <v-container align-center justify-center grid-list-md fill-height>
     <v-btn @click="doScan" color="primary">Scan a tag</v-btn>
+
+    <v-alert class="bottomToast" transition="fade-transition" :value="scanSuccess" type="success">{{$t('qrcode.success')}}</v-alert>
+
   </v-container>
 </template>
 
 <script>
 export default {
   name: "History",
+  data() {
+    return {
+      scanSuccess: false
+    };
+  },
+  watch: {
+    scanSuccess(state) {
+      if (state) {
+        // If state success
+        setTimeout(() => {
+          // After delay reset the state to waiting new scan
+          this.scanSuccess = false;
+        }, 3000);
+      }
+    }
+  },
   methods: {
-    doScan() {}
+    doScan() {
+      this.scanSuccess = true;
+    }
   }
 };
 </script>
