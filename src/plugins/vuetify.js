a962e43a3caf40d313793e2fd03369facbf744ee
Valentin Brosseau <c4software@gmail.com>
2018-10-30 14:04:30 +0100

    Add translation on vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,13 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
+import fr from "vuetify/es5/locale/fr";
+import en from "vuetify/es5/locale/en";
 import "vuetify/src/stylus/app.styl";
 
 Vue.use(Vuetify, {
-  iconfont: "md"
+  iconfont: "md",
+  lang: {
+    locales: { fr, en },
+    current: "fr"
+  }
 });
