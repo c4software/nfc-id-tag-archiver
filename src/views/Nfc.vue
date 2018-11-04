c1e791da7e42666007c36b3e7e6003020704075b
Valentin Brosseau <c4software@gmail.com>
2018-11-04 16:31:38 +0100

    Add NFC cordova code

--- src/views/Nfc.vue
+++ src/views/Nfc.vue
@@ -1,13 +1,26 @@
+// Require Cordova plugin : phonegap-nfc
+
 <template>
   <v-container align-center justify-center grid-list-md fill-height text-xs-center>
 
-    <div>
+    <div v-if="isCompatible && !nfcDisabled">
       <v-fade-transition mode="out-in">
         <v-icon color="success" size="50" v-if="scanSuccess">check_circle</v-icon>
         <v-progress-circular indeterminate size="50" v-else color="primary" />
       </v-fade-transition>
 
       <p class="pa-4">{{$t('nfc.waiting')}}</p>
+    </div>
+    <div v-else-if="nfcDisabled">
+      <p class="pa-4">
+        {{$t('nfc.disabled')}}
+      </p>
+      <v-btn @click="showSettings" color="primary">{{$t('enable')}}</v-btn>
+    </div>
+    <div v-else>
+      <p class="pa-4">
+        {{$t('nfc.incompatible')}}
+      </p>
     </div>
 
     <v-alert class="bottomToast" transition="fade-transition" :value="scanSuccess" type="success">{{$t('nfc.success')}}</v-alert>
@@ -20,15 +33,71 @@ export default {
   name: "nfc",
   data() {
     return {
-      scanSuccess: false
+      scanSuccess: false,
+      isCompatible: true,
+      nfcDisabled: false,
+      items: JSON.parse(localStorage.getItem("nfcScanHistory") || "[]")
     };
   },
   watch: {
-    scanSuccess(state) {
-      if (state) {
-        // If state success
+    items: function(values) {
+      localStorage.setItem("nfcScanHistory", JSON.stringify(values));
+    }
+  },
+  mounted() {
+    this.registerTagEvent();
+  },
+  beforeDestroy() {
+    this.unregisterTagEvent();
+  },
+  methods: {
+    registerTagEvent() {
+      document.removeEventListener("resume", this.registerTagEvent, false);
+
+      if (typeof nfc !== "undefined") {
+        // eslint-disable-next-line
+        nfc.addTagDiscoveredListener(
+          this.scan,
+          this.registerSuccess,
+          this.registerError
+        );
+      } else {
+        this.registerError();
+      }
+    },
+    registerSuccess() {
+      this.isCompatible = true;
+      this.nfcDisabled = false;
+    },
+    unregisterTagEvent() {
+      if (typeof nfc !== "undefined") {
+        // eslint-disable-next-line
+        nfc.removeTagDiscoveredListener(this.scan);
+      }
+    },
+    registerError(e) {
+      if (e === "NFC_DISABLED") {
+        this.isCompatible = true;
+        this.nfcDisabled = true;
+      } else {
+        this.isCompatible = false;
+        this.nfcDisabled = false;
+      }
+    },
+    showSettings() {
+      // eslint-disable-next-line
+      nfc.showSettings();
+      document.addEventListener("resume", this.registerTagEvent, false);
+    },
+    scan(nfcEvent) {
+      let tag = nfcEvent.tag;
+      if (tag) {
+        // eslint-disable-next-line
+        let tagId = nfc.bytesToHexString(tag.id);
+        this.items.push(tagId);
+        this.scanSuccess = true;
+
         setTimeout(() => {
-          // After delay reset the state to waiting new scan
           this.scanSuccess = false;
         }, 3000);
       }
